c903d56a00f1a01ea7b8ac4d18f6921bffca7d3f
John <fang97j@gmail.com>
2020-12-01 16:26:55 -0500

    clean up day 1 code

--- day01/Main.hs
+++ day01/Main.hs
@@ -9,7 +9,7 @@ triples :: [a] -> [[a]]
 triples l = [[x, y, z] | (x:xs) <- tails l, (y:ys) <- tails xs, z <- ys]
 
 solve :: [[Int]] -> Int
-solve l = let ans = (filter (\x -> sum x == 2020) l) !! 0
+solve l = let ans = head $ filter (\x -> sum x == 2020) l
           in foldr (*) 1 ans
 
 main :: IO ()

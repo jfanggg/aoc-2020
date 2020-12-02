b32a3c077bf768579e398c7c6747fd138d04eb49
John <fang97j@gmail.com>
2020-12-02 01:48:15 -0500

    pretty up day 2 code (?)

--- day02/Main.hs
+++ day02/Main.hs
@@ -4,27 +4,27 @@ import Data.List
 import Data.List.Split
 
 processLine :: String -> (Int, Int, Char, String)
-processLine s = let [x, y, z] = words s
-                    [lo, hi] = map read $ splitOn "-" x :: [Int]
-                    c = head y
-                in (lo, hi, c, z)
+processLine s = (lo, hi, c, z)
+  where [x, y, z] = words s
+        [lo, hi] = map read $ splitOn "-" x :: [Int]
+        c = head y
 
 validate1 :: (Int, Int, Char, String) -> Int
-validate1 (lo, hi, c, z) =  let len = length $ filter (== c) z
-                            in fromEnum $ lo <= len && len <= hi
+validate1 (lo, hi, c, s) = fromEnum $ lo <= len && len <= hi
+  where len = length $ filter (== c) s
 
 validate2 :: (Int, Int, Char, String) -> Int
-validate2 (lo, hi, c, z) = fromEnum $ (z !! (lo - 1) == c) /= (z !! (hi - 1) == c)
+validate2 (lo, hi, c, s) = fromEnum $ (s !! (lo - 1) == c) /= (s !! (hi - 1) == c)
 
 main :: IO ()
 main = do
   raw <- readFile "input.txt"
   let ls = map processLine $ lines raw
-      ans1 = foldr (+) 0 $ map validate1 ls
-      ans2 = foldr (+) 0 $ map validate2 ls
 
+  let ans1 = sum $ map validate1 ls
   putStr "Part 1: "
   print ans1
 
+  let ans2 = sum $ map validate2 ls
   putStr "Part 2: "
   print ans2

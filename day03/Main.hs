922b8e394ad2b9152c7aa98e6572e9adf77a2031
John <fang97j@gmail.com>
2020-12-03 00:47:47 -0500

    pretty up day 3 code

--- day03/Main.hs
+++ day03/Main.hs
@@ -2,14 +2,15 @@ module Main where
 
 import Data.List.Split ( chunksOf )
 
-processLine :: String -> [Bool]
-processLine = map (== '#')
+processLine :: String -> [Int]
+processLine = map $ fromEnum . (== '#')
 
-travel :: [[Bool]] -> Int -> Int -> Int
-travel m dr dc = sum $ map (\(idx, row) -> fromEnum $ row !! get_c idx) rows
-  where c' = length $ head m
-        get_c = \x -> dc * x `mod` c'
-        rows = zip [0..] $ map head $ chunksOf dr m
+travel :: [[Int]] -> Int -> Int -> Int
+travel input dr dc = sum collisions
+  where n_col = length $ head input
+        get_c = \x -> dc * x `mod` n_col
+        rows = zip [0..] $ map head $ chunksOf dr input
+        collisions = map (\(idx, row) -> row !! get_c idx) rows
 
 main :: IO ()
 main = do
@@ -19,5 +20,6 @@ main = do
   putStr "Part 1: "
   print $ travel ls 1 3
 
+  let slopes = [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]]
   putStr "Part 2: "
-  print $ product [travel ls 1 1, travel ls 1 3, travel ls 1 5, travel ls 1 7, travel ls 2 1]
+  print $ product $ map (\[dr, dc] -> travel ls dr dc) slopes

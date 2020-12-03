268963570f828066de81b02f6ccde44fb8df34ad
John <fang97j@gmail.com>
2020-12-03 00:33:39 -0500

    solve day 3 part 2

--- day03/Main.hs
+++ day03/Main.hs
@@ -1,13 +1,15 @@
 module Main where
 
+import Data.List.Split ( chunksOf )
+
 processLine :: String -> [Bool]
 processLine = map (== '#')
 
-travel :: [[Bool]] -> Int
-travel m = sum $ map (\(r, row) -> fromEnum $ row !! get_c r) rows
+travel :: [[Bool]] -> Int -> Int -> Int
+travel m dr dc = sum $ map (\(idx, row) -> fromEnum $ row !! get_c idx) rows
   where c' = length $ head m
-        get_c = \r -> 3 * r `mod` c'
-        rows = zip [0..] m
+        get_c = \x -> dc * x `mod` c'
+        rows = zip [0..] $ map head $ chunksOf dr m
 
 main :: IO ()
 main = do
@@ -15,4 +17,7 @@ main = do
   let ls = map processLine $ lines raw
 
   putStr "Part 1: "
-  print $ travel ls
+  print $ travel ls 1 3
+
+  putStr "Part 2: "
+  print $ product [travel ls 1 1, travel ls 1 3, travel ls 1 5, travel ls 1 7, travel ls 2 1]

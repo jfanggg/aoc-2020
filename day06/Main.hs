2698662ecd625818866c0148a9dfa921f7e5022f
John <fang97j@gmail.com>
2020-12-06 00:31:08 -0500

    clean up day 6

--- day06/Main.hs
+++ day06/Main.hs
@@ -3,22 +3,20 @@ module Main where
 import Data.List.Split ( splitOn )
 import qualified Data.Set as S
 
-numQuestions :: String -> Int
-numQuestions s = S.size $ S.fromList filtered
-  where filtered = filter (/= ' ') s
-
-allAnswered :: String -> Int
-allAnswered s = S.size (foldl S.intersection (S.fromList ['a'..'z']) sets)
-  where responses = words s
-        sets = map S.fromList responses
+combine :: (S.Set Char -> S.Set Char -> S.Set Char) -> String -> Int
+combine f s = S.size $ foldl f h t
+  where (h:t) = map S.fromList $ words s
 
 main :: IO ()
 main = do
   raw <- readFile "input.txt"
   let groups = map unwords $ splitOn [""] $ lines raw
 
+  let f = \x -> sum . map (combine x)
+  let [ans1, ans2] = f <$> [S.union, S.intersection] <*> pure groups
+
   putStr "Part 1: "
-  print $ sum $ map numQuestions groups
+  print ans1
 
   putStr "Part 2: "
-  print $ sum $ map allAnswered groups
+  print ans2

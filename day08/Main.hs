01bc3321a5ebf7eb1afe58ed85aee8565e6055ee
John <fang97j@gmail.com>
2020-12-08 01:43:29 -0500

    even more day 8 cleanup

--- day08/Main.hs
+++ day08/Main.hs
@@ -1,9 +1,9 @@
 module Main where
 
 import qualified Data.Set as S
-import Data.List.Split
-import Data.Maybe 
-import Data.Either
+import Data.List.Split ( splitOn )
+import Data.Maybe ( mapMaybe ) 
+import Data.Either ( rights )
 
 data State = State {
   pc :: Int,
@@ -16,27 +16,34 @@ type Instruction = (Operation, Int)
 type Program = [Instruction]
 
 parseOperation :: String -> Operation
-parseOperation "nop" = Nop
-parseOperation "acc" = Acc
-parseOperation "jmp" = Jmp
+parseOperation s = case s of
+  "nop" -> Nop
+  "acc" -> Acc
+  "jmp" -> Jmp
 
 processLine :: String -> Instruction
-processLine s = (parseOperation op', arg)
-  where [op', arg'] = splitOn " " s        
-
-        sign = if head arg' == '+' then 1 else -1
-        arg = sign * (read $ tail arg' :: Int)
+processLine s = (parseOperation op, arg)
+  where [op, sign:num] = splitOn " " s
+        arg = (if sign == '+' then 1 else -1) * (read num :: Int)
 
 stepState :: State -> Instruction -> State
-stepState state (Nop, _)   = State (pc state + 1)   (acc state)
-stepState state (Jmp, val) = State (pc state + val) (acc state)
-stepState state (Acc, val) = State (pc state + 1)   (acc state + val)
+stepState state instruction = case instruction of 
+  (Nop, _)    -> State (pc state + 1)   (acc state)
+  (Jmp, val)  -> State (pc state + val) (acc state)
+  (Acc, val)  -> State (pc state + 1)   (acc state + val)
 
-run :: Program -> State -> S.Set Int -> Either Int Int
-run program state seen = if pc state >= length program then Right (acc state) else recurse
-  where nextState = stepState state (program !! pc state)
-        nextPc = pc nextState 
-        recurse = if S.member nextPc seen then Left (acc state) else run program nextState (S.union seen (S.singleton $ pc state))
+run :: State -> S.Set Int -> Program -> Either Int Int
+run state seen program = res 
+  where terminated = pc state >= length program
+
+        nextState = stepState state (program !! pc state)
+        nextSeen = S.union seen (S.singleton $ pc state)
+        willLoop = S.member (pc nextState) seen
+
+        res
+          | terminated  = Right (acc state) 
+          | willLoop    = Left  (acc state) 
+          | otherwise   = run nextState nextSeen program
 
 alterProgram :: Program -> Int -> Maybe Program
 alterProgram program index = altered
@@ -52,13 +59,14 @@ main = do
   let program = map processLine $ lines raw
 
   let state0 = State 0 0
+  let run0 = run state0 S.empty
 
   putStr "Part 1: "
-  let Left ans1 = run program state0 S.empty
+  let Left ans1 = run0 program
   print ans1
 
   putStr "Part 2: "
   let altered = mapMaybe (alterProgram program) [1..length program - 1]
-  let ans2 = head $ rights $ map (\p -> run p state0 S.empty) altered
+  let ans2 = head $ rights $ map run0 altered
 
   print ans2

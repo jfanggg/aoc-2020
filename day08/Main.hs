f5bc01c04e8247a1dac27742214ab0bf0e1c9f5c
John <fang97j@gmail.com>
2020-12-08 01:18:59 -0500

    more substantial day 8 cleanup

--- day08/Main.hs
+++ day08/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import qualified Data.Set as S
 import Data.List.Split
 import Data.Maybe 
+import Data.Either
 
 data State = State {
   pc :: Int,
@@ -31,22 +32,16 @@ stepState state (Nop, _)   = State (pc state + 1)   (acc state)
 stepState state (Jmp, val) = State (pc state + val) (acc state)
 stepState state (Acc, val) = State (pc state + 1)   (acc state + val)
 
-findLoop :: Program -> State -> S.Set Int -> Maybe Int
-findLoop program state seen = if pc state >= length program then Nothing else recurse
+run :: Program -> State -> S.Set Int -> Either Int Int
+run program state seen = if pc state >= length program then Right (acc state) else recurse
   where nextState = stepState state (program !! pc state)
         nextPc = pc nextState 
-        recurse = if S.member nextPc seen then Just $ acc state else findLoop program nextState (S.union seen (S.singleton $ pc state))
-
-runProgram :: Program -> State -> Int
-runProgram program state = if pc state >= length program then acc state else recurse
-  where nextState = stepState state (program !! pc state)
-        recurse = runProgram program nextState
+        recurse = if S.member nextPc seen then Left (acc state) else run program nextState (S.union seen (S.singleton $ pc state))
 
 alterProgram :: Program -> Int -> Maybe Program
-alterProgram program line = new 
-  where (before,old:after) = splitAt line program
-        (op, val) = old
-        new
+alterProgram program index = altered
+  where (before,(op, val):after) = splitAt index program
+        altered
           | op == Jmp = Just (before ++ (Nop, val) : after)
           | op == Nop = Just (before ++ (Jmp, val) : after)
           | otherwise = Nothing
@@ -56,11 +51,14 @@ main = do
   raw <- readFile "input.txt"
   let program = map processLine $ lines raw
 
+  let state0 = State 0 0
+
   putStr "Part 1: "
-  print $ findLoop program (State 0 0) S.empty
+  let Left ans1 = run program state0 S.empty
+  print ans1
 
   putStr "Part 2: "
   let altered = mapMaybe (alterProgram program) [1..length program - 1]
-  let correct = head $ filter (\p -> Data.Maybe.isNothing (findLoop p (State 0 0) S.empty)) altered
+  let ans2 = head $ rights $ map (\p -> run p state0 S.empty) altered
 
-  print $ runProgram correct (State 0 0)
+  print ans2

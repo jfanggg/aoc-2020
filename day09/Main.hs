4278d961a8d31ef5b75b43924e47f949bfdf2769
John <fang97j@gmail.com>
2020-12-09 21:39:08 -0500

    redo day 9 part 2 with vectors

--- day09/Main.hs
+++ day09/Main.hs
@@ -2,30 +2,24 @@ module Main where
 
 import Data.List ( tails )
 import Data.Maybe ( mapMaybe )
+import qualified Data.Vector as V
 
 preambleLen :: Int
 preambleLen = 25
-
-minInt :: Int
-minInt = minBound
-
-maxInt :: Int
-maxInt = maxBound
 
 checkWindow :: [Int] -> Maybe Int
 checkWindow window = if not valid then Just h else Nothing
   where (h:preamble) = reverse window
         valid = h `elem` [x + y | (x:xs) <- tails preamble, y <- xs]
 
-findRange :: Int -> Int -> Int -> Int -> [Int] -> Maybe Int
-findRange target sumAcc minAcc maxAcc (h:t)  = res
-  where sumAcc' = sumAcc + h
-        minAcc' = min minAcc h
-        maxAcc' = max maxAcc h
-        res 
-          | sumAcc' == target = Just (maxAcc' + minAcc')
-          | sumAcc' >  target = Nothing
-          | otherwise         = findRange target sumAcc' minAcc' maxAcc' t
+findRange :: V.Vector Int -> Int -> Int
+findRange nums target = V.maximum range + V.minimum range
+  where find s e acc 
+          | acc == target = V.slice s (e - s + 1) nums
+          | acc >  target = find (s + 1) e (acc - nums V.! s)
+          | otherwise     = find s (e + 1) (acc + nums V.! e)
+        
+        range = find 0 0 0
 
 main :: IO ()
 main = do
@@ -37,6 +31,5 @@ main = do
   putStr "Part 1: "
   print ans1
 
-  let ans2 = head $ mapMaybe (findRange ans1 0 maxInt minInt) $ tails nums
   putStr "Part 2: "
-  print ans2
+  print $ findRange (V.fromList nums) ans1

90e9e8fa0c49de10d3c5590ec676372b44c732ef
John <fang97j@gmail.com>
2020-12-10 01:25:00 -0500

    clean up day 10

--- day10/Main.hs
+++ day10/Main.hs
@@ -1,49 +1,34 @@
 module Main where
 
-import Data.List
-import Text.Regex.PCRE
-import qualified Data.Set as S
+import Data.List ( sort )
 import qualified Data.Vector as V
 import qualified Data.Map as M
 
--- backtrack :: V.Vector Int -> Int -> Int
--- backtrack nums idx = ans
---   where done = idx == length nums - 1
---         val = nums V.! idx
+countPaths :: M.Map Int Int -> Int -> M.Map Int Int
+countPaths dp num = dpUpdated
+  where val = M.findWithDefault 1 num dp
+        
+        updateKeys = [num + 1 .. num + 3]
+        updateVals = map (val+) $ M.findWithDefault 0 <$> updateKeys <*> pure dp
 
---         ans
---           | done = 1
---           | otherwise = sum $ map (backtrack nums) $ takeWhile (\x -> nums V.! x - val <= 3) [idx + 1 .. length nums - 1]
-
-
-p2 :: V.Vector Int -> M.Map Int Int -> Int -> M.Map Int Int
-p2 nums oldMap idx = newMap
-  where val = nums V.! idx
-        zer = if val `M.member` oldMap then oldMap M.! val else 1
-        one = if (val + 1) `M.member` oldMap then oldMap M.! (val + 1) else 0
-        two = if (val + 2) `M.member` oldMap then oldMap M.! (val + 2) else 0
-        thr = if (val + 3) `M.member` oldMap then oldMap M.! (val + 3) else 0
-
-        newMap'' = M.insert (val + 1) (zer + one) oldMap
-        newMap' = M.insert (val + 2) (zer + two) newMap''
-        newMap = M.insert (val + 3) (zer + thr) newMap'
+        updates = M.fromList $ zip updateKeys updateVals
+        dpUpdated = M.union updates dp
 
 main :: IO ()
 main = do
   raw <- readFile "input.txt"
   let nums = map read $ lines raw :: [Int]
 
-  let sorted = V.fromList $ sort nums
-  let diffs = map (\idx -> sorted V.! (idx + 1) - sorted V.! idx) [0..length sorted - 2]
+  let end = maximum nums + 3
+  let sorted = V.fromList $ [0] ++ sort nums ++ [end]
 
-  let ones = length $ filter (== 1) diffs
+  let diffs = map (\idx -> sorted V.! (idx + 1) - sorted V.! idx) [0..length sorted - 2]
+  let ones   = length $ filter (== 1) diffs
   let threes = length $ filter (== 3) diffs
 
   putStr "Part 1: "
-  print $ (ones +1) * (threes+1)
+  print $ ones * threes
 
-  let end = maximum nums + 3
-  let list2 = V.fromList $ [0] ++ sort nums ++ [end]
-
+  let dp = foldl countPaths M.empty sorted
   putStr "Part 2: "
-  print $ (foldl (p2 list2) M.empty [0..length list2 - 1]) M.! end
+  print $ dp M.! end

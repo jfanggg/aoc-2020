f5982b0ebcc3c63a56f3872a5f49c9165846e71c
John <fang97j@gmail.com>
2020-12-10 14:19:53 -0500

    change variable name in day 10

--- day10/Main.hs
+++ day10/Main.hs
@@ -5,13 +5,13 @@ import qualified Data.Vector as V
 import qualified Data.Map as M
 
 countPaths :: M.Map Int Int -> Int -> M.Map Int Int
-countPaths dp num = updated
-  where val = M.findWithDefault 0 num dp
+countPaths dp key = updated
+  where val = M.findWithDefault 0 key dp
         
-        updateKeys = [num + 1 .. num + 3]
+        updateKeys = [key + 1 .. key + 3]
         updateVals = map (val+) $ M.findWithDefault 0 <$> updateKeys <*> pure dp
 
-        updated = M.fromList $ zip (num:updateKeys) (val:updateVals)
+        updated = M.fromList $ zip (key:updateKeys) (val:updateVals)
 
 main :: IO ()
 main = do

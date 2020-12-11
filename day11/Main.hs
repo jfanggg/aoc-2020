1648b33da0c7120711b4fb8f23463d3f9a3653e1
John <fang97j@gmail.com>
2020-12-11 01:17:54 -0500

    clean up day 11

--- day11/Main.hs
+++ day11/Main.hs
@@ -1,16 +1,13 @@
 module Main where
 
-import Text.Regex.PCRE
-import qualified Data.Set as S
-import qualified Data.Map as M
+import Data.List
 import qualified Data.Vector as V
-import Debug.Trace
 import Data.Maybe ( mapMaybe )
+import Debug.Trace
 
 data Status = F | O | E deriving (Show, Eq);
 type Grid = V.Vector (V.Vector Status)
-
--- myfun a b | trace ("myfun " ++ show a ++ " " ++ show b) False = undefined
+type Vec2 = (Int, Int)
 
 processChar :: Char -> Status
 processChar c = case c of 
@@ -18,84 +15,58 @@ processChar c = case c of
   '.' -> F
   '#' -> O
 
-deltas :: [(Int, Int)]
-deltas = [(1,0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
+deltas :: [Vec2]
+deltas = [(r, c) | r <- [-1..1], c <- [-1..1], not (r == 0 && c == 0)]
 
-readAt :: Grid -> (Int, Int) -> Maybe Status
-readAt grid (r, c) = val
-  where validR = 0 <= r && r < V.length grid
-        validC = 0 <= c && c < V.length (grid V.! 0)
+look' :: Bool -> Grid -> Vec2 -> Vec2 -> Maybe Status
+look' extend grid (r, c) (dR, dC) = res
+  where val = grid V.!? (r + dR) >>= (V.!? (c + dC))
 
-        val 
-          | validR && validC  = Just $ (grid V.! r) V.! c 
-          | otherwise         = Nothing
+        res 
+          | extend && val == Just F = look' extend grid (r + dR, c + dC) (dR, dC)
+          | otherwise               = val
 
-updatePosition :: Grid -> (Int, Int) -> Status
-updatePosition grid (r, c) = updated
-  where Just myStatus = readAt grid (r, c)
-        surroundings = mapMaybe (\ (dR, dC) -> readAt grid (r + dR, c + dC)) deltas
+look :: Grid -> Vec2 -> Vec2 -> Maybe Status
+look = look' False
 
-        updated = case myStatus of 
-          E -> if O `notElem` surroundings then O else E
-          O -> if length (filter (== O) surroundings) >= 4 then E else O
-          F -> F
+lookExtend :: Grid -> Vec2 -> Vec2 -> Maybe Status
+lookExtend = look' True
 
-readAtExtend :: Grid -> (Int, Int) -> (Int, Int) -> Maybe Status
-readAtExtend grid (r, c) (dR, dC) = val
-  where validR = 0 <= r && r < V.length grid
-        validC = 0 <= c && c < V.length (grid V.! 0)
+updatePosition :: Bool -> Grid -> (Int, Int) -> Status
+updatePosition p2 grid pos = updated
+  where threshold = if p2 then 5 else 4
+        lookFunc = if p2 then lookExtend else look
 
-        val 
-          | validR && validC  = if (grid V.! r) V.! c == F then readAtExtend grid (r + dR, c + dC) (dR, dC) else Just $ (grid V.! r) V.! c 
-          | otherwise         = Nothing
-
-updatePosition2 :: Grid -> (Int, Int) -> Status
-updatePosition2 grid (r, c) = updated
-  where Just myStatus = readAt grid (r, c)
-        surroundings = mapMaybe (\ (dR, dC) -> readAtExtend grid (r + dR, c + dC) (dR, dC)) deltas
+        Just myStatus = look grid pos (0, 0)
+        surroundings = mapMaybe (lookFunc grid pos) deltas
 
         updated = case myStatus of 
           E -> if O `notElem` surroundings then O else E
-          O -> if length (filter (== O) surroundings) >= 5 then E else O
+          O -> if length (filter (== O) surroundings) >= threshold then E else O
           F -> F
 
-step :: Grid -> Grid
-step grid = V.fromList $ map mapR [0..num_r - 1]
+step :: Bool -> Grid -> Grid
+step p2 grid = V.fromList $ map mapR [0..num_r - 1]
   where num_r = V.length grid
         num_c = V.length (grid V.! 0)
 
-        update = curry $ updatePosition grid 
+        update = curry $ updatePosition p2 grid 
         mapR = \r -> V.fromList $ map (update r) [0..num_c - 1]
 
-step2 :: Grid -> Grid
-step2 grid = V.fromList $ map mapR [0..num_r - 1]
-  where num_r = V.length grid
-        num_c = V.length (grid V.! 0)
-
-        update = curry $ updatePosition2 grid 
-        mapR = \r -> V.fromList $ map (update r) [0..num_c - 1]
-
-simulate :: Grid -> Grid
-simulate old = if old /= new then simulate new else old
-  where new = step old
-
-simulate2 :: Grid -> Grid
-simulate2 old = if old /= new then simulate2 new else old
-  where new = step2 old
+findEquilibrium :: Bool -> Grid -> Grid
+findEquilibrium p2 old = if old /= new then findEquilibrium p2 new else old
+  where new = step p2 old
 
 main :: IO ()
 main = do
   raw <- readFile "input.txt"
   let grid = V.fromList $ map (V.fromList . map processChar) $ lines raw
+  let countOccupancies = sum . V.map (length . V.filter (==O))
 
-  let fixed = simulate grid
-  let ans1 = sum $ V.map (length . V.filter (==O)) fixed
+  let ans1 = countOccupancies $ findEquilibrium False grid
   putStr "Part 1: "
   print ans1 
 
-  let fixed2 = simulate2 grid
-  let ans2 = sum $ V.map (length . V.filter (==O)) fixed2
+  let ans2 = countOccupancies $ findEquilibrium True grid
   putStr "Part 2: "
   print ans2
-
-  print ""

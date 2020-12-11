a56f748379322edd2694258430ac8b9e08ddf655
John <fang97j@gmail.com>
2020-12-11 00:36:05 -0500

    solve day 11 part 2 with copy/paste

--- day11/Main.hs
+++ day11/Main.hs
@@ -40,6 +40,25 @@ updatePosition grid (r, c) = updated
           O -> if length (filter (== O) surroundings) >= 4 then E else O
           F -> F
 
+readAtExtend :: Grid -> (Int, Int) -> (Int, Int) -> Maybe Status
+readAtExtend grid (r, c) (dR, dC) = val
+  where validR = 0 <= r && r < V.length grid
+        validC = 0 <= c && c < V.length (grid V.! 0)
+
+        val 
+          | validR && validC  = if (grid V.! r) V.! c == F then readAtExtend grid (r + dR, c + dC) (dR, dC) else Just $ (grid V.! r) V.! c 
+          | otherwise         = Nothing
+
+updatePosition2 :: Grid -> (Int, Int) -> Status
+updatePosition2 grid (r, c) = updated
+  where Just myStatus = readAt grid (r, c)
+        surroundings = mapMaybe (\ (dR, dC) -> readAtExtend grid (r + dR, c + dC) (dR, dC)) deltas
+
+        updated = case myStatus of 
+          E -> if O `notElem` surroundings then O else E
+          O -> if length (filter (== O) surroundings) >= 5 then E else O
+          F -> F
+
 step :: Grid -> Grid
 step grid = V.fromList $ map mapR [0..num_r - 1]
   where num_r = V.length grid
@@ -48,9 +67,21 @@ step grid = V.fromList $ map mapR [0..num_r - 1]
         update = curry $ updatePosition grid 
         mapR = \r -> V.fromList $ map (update r) [0..num_c - 1]
 
+step2 :: Grid -> Grid
+step2 grid = V.fromList $ map mapR [0..num_r - 1]
+  where num_r = V.length grid
+        num_c = V.length (grid V.! 0)
+
+        update = curry $ updatePosition2 grid 
+        mapR = \r -> V.fromList $ map (update r) [0..num_c - 1]
+
 simulate :: Grid -> Grid
 simulate old = if old /= new then simulate new else old
   where new = step old
+
+simulate2 :: Grid -> Grid
+simulate2 old = if old /= new then simulate2 new else old
+  where new = step2 old
 
 main :: IO ()
 main = do
@@ -61,5 +92,10 @@ main = do
   let ans1 = sum $ V.map (length . V.filter (==O)) fixed
   putStr "Part 1: "
   print ans1 
+
+  let fixed2 = simulate2 grid
+  let ans2 = sum $ V.map (length . V.filter (==O)) fixed2
+  putStr "Part 2: "
+  print ans2
 
   print ""

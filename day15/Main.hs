db2edc5240ecc53aa5515d99e0dbe8c4c1596362
John <fang97j@gmail.com>
2020-12-19 17:43:38 -0500

    clean up day 15

--- day15/Main.hs
+++ day15/Main.hs
@@ -3,20 +3,23 @@ module Main where
 import           Control.Monad               (forM_, foldM_, when)
 import qualified Data.Vector.Unboxed.Mutable as V
 
+n :: Int
+n = 30000000
+
 main :: IO ()
 main = do
   let input = [0, 6, 1, 7, 2, 19, 20]
-  let (h, [t]) = splitAt (length input - 1) input
+  let (start, [v]) = splitAt (length input - 1) input
 
-  memory <- V.replicate 30000000 (-1 :: Int)
-  forM_ (zip h [1..]) (uncurry $ V.write memory)
+  memory <- V.replicate n (-1 :: Int)
+  forM_ (zip start [1..]) (uncurry $ V.write memory)
 
-  let loop val t = do
-      when (t == 2020)      $ putStrLn $ "Part 1: " ++ show val
-      when (t == 30000000)  $ putStrLn $ "Part 2: " ++ show val
+  let loop val time = do
+      when (time == 2020) $ putStrLn $ "Part 1: " ++ show val
+      when (time == n)    $ putStrLn $ "Part 2: " ++ show val
 
-      l <- V.read memory val
-      V.write memory val t
-      return (if l == -1 then 0 else t - l)
+      lookup <- V.read memory val
+      V.write memory val time
+      return (if lookup == -1 then 0 else time - lookup)
 
-  foldM_ loop t [length input..30000000]
+  foldM_ loop v [length input..n]

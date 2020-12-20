eaa079a632c79313bd8ae3758930436a2da2553c
John <fang97j@gmail.com>
2020-12-20 16:41:58 -0500

    solve day 17 part 2

--- day17/Main.hs
+++ day17/Main.hs
@@ -4,12 +4,13 @@ import qualified Data.Map as M
 import Debug.Trace
 
 data Status = A | I deriving (Show, Eq);
-data Vec3 = Vec3 {
+data Vec4 = Vec4 {
   getX :: Int,
   getY :: Int,
-  getZ :: Int
+  getZ :: Int,
+  getW :: Int
 } deriving (Show, Eq, Ord);
-type Grid = M.Map Vec3 Status
+type Grid = M.Map Vec4 Status
 
 maxInt :: Int
 maxInt = maxBound
@@ -22,27 +23,31 @@ parseStatus c = case c of
   '#' -> A
   '.' -> I
 
-deltas :: [Vec3]
-deltas = [Vec3 x y z | x <- [-1..1], y <- [-1..1], z <- [-1..1], not (x == 0 && y == 0 && z == 0)]
+deltas :: [Vec4]
+deltas = [Vec4 x y z w |  x <- [-1..1], y <- [-1..1], z <- [-1..1], w <- [-1..1], 
+                        not (x == 0 && y == 0 && z == 0 && w == 0)]
 
 parseRow :: Int -> String -> Grid
 parseRow r row = M.fromList points
-  where points = zipWith (\c val -> (Vec3 r c 0, parseStatus val)) [0..] row
+  where points = zipWith (\c val -> (Vec4 r c 0 0, parseStatus val)) [0..] row
 
-axisRange :: Grid -> (Vec3 -> Int) -> [Int]
+axisRange :: Grid -> (Vec4 -> Int) -> [Int]
 axisRange grid getter = [lo - 1 .. hi + 1]
   where func vec _ (lo, hi) = let val = getter vec in (min lo val, max hi val)
         acc = (maxInt, minInt)
         (lo, hi) = M.foldrWithKey func acc grid
 
-readAt :: Grid -> Vec3 -> Vec3 -> Status
+addVec4 :: Vec4 -> Vec4 -> Vec4
+addVec4 a b = Vec4 (getX a + getX b) (getY a + getY b) (getZ a + getZ b) (getW a + getW b)
+
+readAt :: Grid -> Vec4 -> Vec4 -> Status
 readAt grid pos dir = status
-  where newPos = Vec3 (getX pos + getX dir) (getY pos + getY dir) (getZ pos + getZ dir)
+  where newPos = addVec4 pos dir
         status = M.findWithDefault I newPos grid
 
-stepPosition :: Grid -> Vec3 -> Status
+stepPosition :: Grid -> Vec4 -> Status
 stepPosition grid pos = status
-  where me = readAt grid pos (Vec3 0 0 0)
+  where me = readAt grid pos (Vec4 0 0 0 0)
         neighbors = map (readAt grid pos) deltas
         neighborsA = length $ filter (== A) neighbors
 
@@ -50,10 +55,11 @@ stepPosition grid pos = status
           A -> if neighborsA == 2 || neighborsA == 3 then A else I
           I -> if neighborsA == 3 then A else I
 
-stepGrid :: Grid -> Grid
-stepGrid old = new
-  where [xRange, yRange, zRange] = axisRange old <$> [getX, getY, getZ]
-        coords = [Vec3 x y z | x <- xRange, y <- yRange, z <- zRange]
+stepGrid :: Bool -> Grid -> Grid
+stepGrid p2 old = new
+  where [xRange, yRange, zRange, wRange] = axisRange old <$> [getX, getY, getZ, getW]
+        ws = if p2 then wRange else [0]
+        coords = [Vec4 x y z w | x <- xRange, y <- yRange, z <- zRange, w <- ws]
 
         new = M.fromList $ map (\c -> (c, stepPosition old c)) coords
 
@@ -63,7 +69,10 @@ countA = length . M.filter (== A)
 main :: IO ()
 main = do
   raw <- readFile "input.txt"
-
   let grid0 = M.unions $ zipWith parseRow [0..] $ lines raw
-  let ans1 = countA $ iterate stepGrid grid0 !! 6
+
+  let ans1 = countA $ iterate (stepGrid False) grid0 !! 6
   putStrLn $ "Part 1: " ++ show ans1
+
+  let ans2 = countA $ iterate (stepGrid True) grid0 !! 6
+  putStrLn $ "Part 2: " ++ show ans2

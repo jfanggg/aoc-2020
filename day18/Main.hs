f0564bc44fae7ce96bd12b0d0a3f6a4162543a88
John <fang97j@gmail.com>
2020-12-22 21:55:31 -0500

    solve day 18 part 2

--- day18/Main.hs
+++ day18/Main.hs
@@ -12,22 +12,25 @@ eval stack (h:t)
   | h == '+'            = let (f:s:r) = stack in eval (f+s:r) t
 
 -- shuntingYard output operator tokens | trace ("shuntingYard " ++ show tokens ++ " " ++ show output ++ " " ++ show operator) False = undefined
-shuntingYard :: [Char] -> [Char] -> String -> String
-shuntingYard output opStack [] = reverse output ++ opStack
-shuntingYard output opStack (h:t) 
-  | h `elem` ['0'..'9'] = shuntingYard (h:output) opStack t
-  | h `elem` ['+', '*'] = let ops = takeWhile (`elem` ['+', '*']) opStack in 
-                          shuntingYard (reverse ops ++ output) (h:drop (length ops) opStack)t
-  | h == '('            = shuntingYard output (h:opStack) t
+shuntingYard :: Bool -> [Char] -> [Char] -> String -> String
+shuntingYard p2 output opStack [] = reverse output ++ opStack
+shuntingYard p2 output opStack (h:t) 
+  | h `elem` ['0'..'9'] = shuntingYard p2 (h:output) opStack t
+  | h == '+'            = let ops = takeWhile (`elem` if p2 then ['+'] else ['+', '*']) opStack in 
+                          shuntingYard p2 (reverse ops ++ output) (h:drop (length ops) opStack)t
+  | h == '*'            = let ops = takeWhile (`elem` ['+', '*']) opStack in 
+                          shuntingYard p2 (reverse ops ++ output) (h:drop (length ops) opStack)t
+  | h == '('            = shuntingYard p2 output (h:opStack) t
   | h == ')'            = let ops = takeWhile (/= '(') opStack in
-                          shuntingYard (reverse ops ++ output) (drop (length ops + 1) opStack) t
+                          shuntingYard p2 (reverse ops ++ output) (drop (length ops + 1) opStack) t
 
 main :: IO ()
 main = do
   raw <- readFile "input.txt"
   let exprs =  map (filter (/= ' ')) $ lines raw 
   
-  let ans1 = sum $ map (eval [] . shuntingYard [] []) exprs
+  let ans1 = sum $ map (eval [] . shuntingYard False [] []) exprs
   putStrLn $ "Part 1: " ++ show ans1
 
-  print ""
+  let ans2 = sum $ map (eval [] . shuntingYard True [] []) exprs
+  putStrLn $ "Part 2: " ++ show ans2
